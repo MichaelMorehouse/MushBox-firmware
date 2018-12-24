4ac2652efd6f1b2f0f787c7652b758a107fb1605
Michael M <mimomiller@gmail.com>
2018-12-23 18:07:13 -0800

    add device class

--- MushBox.ino
+++ MushBox.ino
@@ -1,6 +1,9 @@
+// Servo library
 #include <Servo.h>
 // Temperature and humidity sensor library
 #include "DHT.h"
+
+#define DHTTYPE DHT11   // [DHT11, DHT22, DHT21]
 
 // Pin definitions
 const int DHTPIN = 2;       // DHT sensor data pin
@@ -9,7 +12,6 @@ const int HOTFAN = 7;       // [PWM] PWM fan control output pin for hot air fan
 const int MIST = 8;    // 12v atomister control output pin
 const int PELTIER = 12; // 12v Peltier Plate relay output pin
 
-#define DHTTYPE DHT11   // [DHT11, DHT22, DHT21]
 DHT dht(DHTPIN, DHTTYPE);
 Servo baffleServo;
 
@@ -17,22 +19,95 @@ Servo baffleServo;
 const float humidityLower = 50;
 const float humidityUpper = 90;
 const float tempLower = 22;
-const float tempUpper = 26;
+const float tempUpper = 23;
 
 int isHumidityAboveLimit = 0;
 int isHumidityBelowLimit = 0;
 int isTempAboveLimit = 0;
 int isTempBelowLimit = 0;
 
-int isMistOn = 0;
-int isCoolFanOn = 0;
-int isHotFanOn = 0;
-int isPeltierOn = 0;
 int isBaffleOpen = 0;
 int airState = 0 ; // 0 - fans off, 1 - ventilating (no AC), 2 - cooling, 3 - heating
 
 long mistOnTime = 0;
 int readingsTaken = 0;
+
+class Device {
+  private:
+    String _name;
+    int _pin, _pinmode, _isOn;
+  public:
+    Device(String name, int pin, int pinmode = 0) {
+      _name = name;
+      _pin = pin;
+      _pinmode = pinmode;
+      _isOn = 0;
+    }
+    String getName() {
+      return _name;
+    }
+    int getPin() {
+      return _pin;
+    }
+    int getPinmode() {
+      return _pinmode;
+    }
+    int isOn() {
+      return _isOn;
+    }
+    void setupDevice() {
+      String mode;
+      switch (_pinmode) {
+        case 0:
+          pinMode(_pin, OUTPUT);
+          mode = "output";
+          break;
+        case 1:
+          pinMode(_pin, INPUT);
+          mode = "input";
+          break;
+        case 2:
+          pinMode(_pin, INPUT_PULLUP);
+          mode = "input pullup";
+          break;
+        default:
+          mode = "PINMODE ERROR!";
+          break;
+      }
+      Serial.print("Set up device ");
+      Serial.print(_name);
+      Serial.print(" at pin ");
+      Serial.print(_pin);
+      Serial.print(" with pinmode ");
+      Serial.println(mode);
+    }
+    void on() {
+      if(!_isOn) {
+        digitalWrite(_pin, HIGH);
+        _isOn = 1;
+      }
+    }
+    void off() {
+      if(_isOn) {
+        digitalWrite(_pin, LOW);
+        _isOn = 0;
+      }
+    }
+    void printStatusToSerial() {
+      String s;
+      s += _name;
+      _isOn ? s += " is ON. " : s += " is OFF. ";
+      Serial.println(s);
+    }
+    void printSetupToSerial() {
+      String s;
+      s += "Pin ";
+      s += _pin;
+      s += " pinmode ";
+      s += _pinmode;
+      Serial.println(s);
+    }
+};
 
 class DHTReading {
   private:
@@ -64,47 +139,19 @@ class DHTReading {
     }
 };
 
-void setup() {
-  Serial.begin(9600);
-  delay(50);
-  Serial.print("Setting up...");
+Device coolfan("cool fan", COOLFAN);
+Device hotfan("hot fan", HOTFAN);
+Device mist("mist", MIST);
+Device peltier("peltier", PELTIER);
 
-  pinMode(COOLFAN, OUTPUT);
-  pinMode(HOTFAN, OUTPUT);
-  pinMode(MIST, OUTPUT);
-  pinMode(PELTIER, OUTPUT);
-
-  //  baffleServo.attach(9);
-  dht.begin();
-  Serial.println("complete.");
-  delay(100);
-}
-
-void loop() {
-  DHTReading reading;
-  float humidity = reading.getHum();
-  float temperature = reading.getTemp();
-
-  // Check if any reads failed and exit early (to try again).
-  if (!reading.isValid()) {
-    Serial.println("Failed to read from DHT sensor!");
-    Serial.println("Waiting to try again...");
-    delay(5000);
-    Serial.println("Re-reading...");
-    return;
-  }
-
-  reading.printToSerial();
-  Serial.println(readingsTaken);
-  humidityCheck(humidity);
-  tempCheck(temperature);
-
-  determineAirState();
-  setACDeviceState();
-
-  // Delay between measurements
-  delayMinutes(.2);
-}
+Device deviceList[] = {
+  coolfan,
+  hotfan,
+  mist,
+  peltier
+};
+// !Must match number of devices added to device list!
+const int deviceNumber = 4;
 
 void delayMinutes(float minutes) {
   float delaymillis = minutes * 60000;
@@ -171,108 +218,7 @@ void determineAirState() {
   }
 }
 
-void setACDeviceState() {
-  switch (airState) {
-    // All off
-    case 0:
-      peltierOff();
-      coolFanOff();
-      hotFanOff();
-      break;
-    // No AC, fans on
-    case 1:
-      peltierOff();
-      coolFanOn();
-      hotFanOn();
-      baffleClose();
-      break;
-    // Air cooling, heat vented
-    case 2:
-      peltierOn();
-      coolFanOn();
-      hotFanOn();
-      baffleOpen();
-      break;
-    // Air heating
-    case 3:
-      peltierOn();
-      coolFanOff();
-      hotFanOn();
-      baffleClose();
-      break;
-    default:
-      break;
-  }
-}
-
-// Atomister methods
-void mistToggle() {
-  Serial.print("Toggling mist: ");
-  isMistOn ? mistOff() : mistOn();
-  return;
-}
-
-void mistOn() {
-  Serial.println("Mist on.");
-  digitalWrite(MIST, HIGH);
-  return;
-}
-
-void mistOff() {
-  Serial.println("Mist off.");
-  digitalWrite(MIST, LOW);
-  return;
-}
-
-// Fan methods
-void coolFanToggle() {
-  isCoolFanOn ? coolFanOff() : coolFanOn();
-  return;
-}
-
-void coolFanOn() {
-  digitalWrite(COOLFAN, HIGH);
-  return;
-}
-
-void coolFanOff() {
-  digitalWrite(COOLFAN, LOW);
-  return;
-}
-
-void hotFanToggle() {
-  isHotFanOn ? hotFanOff() : hotFanOn();
-  return;
-}
-
-void hotFanOn() {
-  digitalWrite(HOTFAN, HIGH);
-  return;
-}
-
-void hotFanOff() {
-  digitalWrite(HOTFAN, LOW);
-  return;
-}
-
-// Peltier plate methods
-void peltierToggle() {
-  isPeltierOn ? peltierOff() : peltierOn();
-  return;
-}
-
-void peltierOn() {
-  digitalWrite(PELTIER, HIGH);
-  return;
-}
-
-void peltierOff() {
-  digitalWrite(PELTIER, LOW);
-  return;
-}
-
 // Baffle methods
-
 void baffleOpen() {
   if (!isBaffleOpen) {
     Serial.println("Opening baffle!");
@@ -287,4 +233,54 @@ void baffleClose() {
     isBaffleOpen = 0;
   }
   return;
+}
+
+void setup() {
+  Serial.begin(9600);
+  delay(50);
+  Serial.println("Setting up...");
+
+  for(int i = 0; i < deviceNumber; i++) {
+    deviceList[i].setupDevice();
+  };
+  //  baffleServo.attach(9);
+  dht.begin();
+  Serial.println("Setup complete.");
+  delay(100);
+}
+
+void loop() {
+  DHTReading reading;
+  float humidity = reading.getHum();
+  float temperature = reading.getTemp();
+
+  // Check if any reads failed and exit early (to try again).
+  if (!reading.isValid()) {
+    Serial.println("Failed to read from DHT sensor!");
+    Serial.println("Waiting to try again...");
+    delay(5000);
+    Serial.println("Re-reading...");
+    return;
+  }
+  
+
+  Serial.print("\nReading #");
+  Serial.println(readingsTaken);
+  reading.printToSerial();
+  
+  humidityCheck(humidity);
+  tempCheck(temperature);
+
+  determineAirState();
+  
+  // Iterate through devices, change limit to reflect # of devices
+  for (int i = 0; i < 4; i++) {
+    deviceList[i].printStatusToSerial();
+  };
+
+  Serial.print("Baffle open: ");
+  Serial.println(isBaffleOpen);  
+
+  // Delay between measurements
+  delayMinutes(.2);
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+#MushBox
+
+Arduino sketches to power the environmental controls for the MushBox mushroom growing enclosure.
